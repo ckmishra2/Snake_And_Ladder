3b278bcf6f635846a6cc819e9929fdc3f4dccbd2
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-06 18:51:16 +0530

    Check for option that player move ahead,back or stay at there position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,13 +10,32 @@ class Player{
 		int dieOutCome = (int)(Math.floor(Math.random() *10)%6)+1;
 		return dieOutCome;
 	} 
+	int checkOption() { 
+		int check =  (int)(Math.floor(Math.random() *10)%3);
+		return check;
+	}
+	
 	//The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number
+	// checks for a Option that players are No Play,Ladder or snake
 }
 public class SnakeAndLadder {
 	public static void main( String[] args )
 	{
 		Player P1= new Player();
 		int result = P1.rollADie();	
+		int check = P1.checkOption();
+		System.out.println(check);
+		switch (check) {
+		case 0:
+			System.out.println("No play Player stay in the same position");
+			break;
+		case 1:
+			System.out.println("The player moves ahead by the number of position received in the die");
+			break;
+		case 2:
+			System.out.println("This is Snake: The player moves behind by the number of position received in die");
+			break;
+		}
 		System.out.println(P1.current_position);  
 		System.out.println(result);
 	}
