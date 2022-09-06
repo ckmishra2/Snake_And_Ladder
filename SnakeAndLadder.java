a9a0a742b87d07096e56af9559819c8aa807daff
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-06 18:12:04 +0530

    Use random() to roll a die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,16 +2,22 @@ package basics;
 
 
 //Snake and Ladder game played with single player at start position 0
-//Snake and Ladder game played with single player at start position 0
 class Player{
-	int current_position = 0;	
+	int current_position = 0;
+	Player(){}
+	
+	int rollADie() {
+		int dieOutCome = (int)(Math.floor(Math.random() *10)%6)+1;
+		return dieOutCome;
+	} 
+	//The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number
 }
 public class SnakeAndLadder {
-public static void main( String[] args )
-{
-	   Player P1= new Player();
-	   
-	   System.out.println(P1.current_position);
-	   
-}
+	public static void main( String[] args )
+	{
+		Player P1= new Player();
+		int result = P1.rollADie();	
+		System.out.println(P1.current_position);  
+		System.out.println(result);
+	}
 }
